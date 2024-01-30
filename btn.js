9758a461a1d95c1a66f8285d3557d357e69a1f5b
Dalet <104555136+Spod101@users.noreply.github.com>
2024-01-30 20:34:08 +0800

    Add files via upload

--- btn.js
+++ btn.js
@@ -12,45 +12,53 @@ function myMove2(Day2){
 let id = null;
 function myMove(day) {
 
-var elem = document.getElementById(day);   
-myMove2(day);
-let pos = -450;
-clearInterval(id);
-id = setInterval(frame, 5);
-function frame() {
-if (pos != 160) {
-document.getElementById(day).style.transition = 'all 0.9s';
-if(day == "Monday"){
-document.getElementById("Monday").style.borderColor = "blueviolet";
-document.getElementById("Monday").style.backgroundColor = "blueviolet";
-}else if(day == "Tuesday"){
-document.getElementById("Tuesday").style.borderColor = "violet";
-document.getElementById("Tuesday").style.backgroundColor = "violet";
-}else if(day == "Wednesday"){
-document.getElementById("Wednesday").style.borderColor = "blue";
-document.getElementById("Wednesday").style.backgroundColor = "blue";
-}else if(day == "Thursday"){
-document.getElementById("Thursday").style.borderColor = "greenyellow";
-document.getElementById("Thursday").style.backgroundColor = "greenyellow";
-}else if(day == "Friday"){
-document.getElementById("Friday").style.borderColor = "yellow";
-document.getElementById("Friday").style.backgroundColor = "yellow";
-}else if(day == "Saturday"){
-document.getElementById("Saturday").style.borderColor = "orange";
-document.getElementById("Saturday").style.backgroundColor = "orange";
-}else if(day == "Sunday"){
-document.getElementById("Sunday").style.borderColor = "red";
-document.getElementById("Sunday").style.backgroundColor = "red";
-}
+    var elem = document.getElementById(day);   
+    myMove2(day);
+    let pos = -450;
+    clearInterval(id);
+    id = setInterval(frame, 5);
 
-pos++;
-pos++;
-elem.style.top = pos + 'px'; 
-elem.style.bottom = pos + 'px';
-} else {
-document.getElementById(day).style.backgroundColor = "white";
-clearInterval(id);	  
+    function frame() {
+        if (pos != 160) {
+            document.getElementById(day).style.transition = 'all 0.9s';
+            if(day == "Monday"){
+                document.getElementById("Monday").style.borderColor = "blueviolet";
+                document.getElementById("Monday").style.backgroundColor = "blueviolet";
+            }
+            else if(day == "Tuesday"){
+                document.getElementById("Tuesday").style.borderColor = "violet";
+                document.getElementById("Tuesday").style.backgroundColor = "violet";
+            }
+            else if(day == "Wednesday"){
+                document.getElementById("Wednesday").style.borderColor = "blue";
+                document.getElementById("Wednesday").style.backgroundColor = "blue";
+            }
+            else if(day == "Thursday"){
+                document.getElementById("Thursday").style.borderColor = "greenyellow";
+                document.getElementById("Thursday").style.backgroundColor = "greenyellow";
+            }
+            else if(day == "Friday"){
+                document.getElementById("Friday").style.borderColor = "yellow";
+                document.getElementById("Friday").style.backgroundColor = "yellow";
+            }
+            else if(day == "Saturday"){
+                document.getElementById("Saturday").style.borderColor = "orange";
+                document.getElementById("Saturday").style.backgroundColor = "orange";
+            }
+            else if(day == "Sunday"){
+                document.getElementById("Sunday").style.borderColor = "red";
+                document.getElementById("Sunday").style.backgroundColor = "red";
+            }
 
-}
-}
+            pos++;
+            pos++;
+            elem.style.top = pos + 'px'; 
+            elem.style.bottom = pos + 'px';
+        } 
+
+        else {
+            document.getElementById(day).style.backgroundColor = "transparent";
+            clearInterval(id);	  
+        }
+    }
 }
